c9a7d44d84965230156558f4a24d9a56937609de
carlosmspk <carlos_cp_santos@hotmail.com>
2022-09-12 21:28:26 +0100

    Refactor generic Error enum to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-pub mod screens;
 pub(crate) mod cli_display;
-pub(crate) mod terminal;
+pub mod error;
+pub mod screens;
+/// make public to crate only later
+pub mod terminal;
 pub(crate) mod text;
+pub mod debug;
 
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
