5226d8a04e0a9f86d6737d28288be944578c5602
carlosmspk <carlos_cp_santos@hotmail.com>
2022-09-19 22:04:15 +0100

    Add simple query module, and initial implementation for struct SimpleQuery

--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -1,2 +1,4 @@
+pub mod simple_query;
 pub mod menu;
+pub use simple_query::SimpleQuery;
 pub use menu::Menu;
---
+++ src/screens/simple_query.rs
@@ -0,0 +1,15 @@
+use crate::{cli_display::Displayable, error::Error, input::{UserInteractable, UserInput}, text::AnyString};
+pub struct SimpleQuery {
+    message_prompt: AnyString,
+    user_response_buffer: String,
+}
+
+impl SimpleQuery {
+    pub fn new(message_prompt: AnyString) -> SimpleQuery {
+        SimpleQuery {
+            message_prompt,
+            user_response_buffer: String::new(),
+        }
+    }
+}
+
