5113b9f04cc99ef76c4cecf7742d783fded55961
carlosmspk <carlos_cp_santos@hotmail.com>
2022-09-19 22:03:05 +0100

    Fixed bad struct usage. Finished implementing as char

--- src/input.rs
+++ src/input.rs
@@ -3,27 +3,30 @@ use crate::error::Error;
 pub enum UserInput {
     Number(i64),
     Text(String),
-    KeyPress(crossterm::event::KeyCode),
-    Mouse(crossterm::event::MouseEventKind),
+    KeyEvent(crossterm::event::KeyEvent),
+    MouseEvent(crossterm::event::MouseEvent),
     Other(crossterm::event::Event),
 }
 
 impl UserInput {
     pub fn as_char(&self) -> Option<char> {
-        if let Self::KeyPress(code) = self {
-            if let crossterm::event::KeyCode::Char(c) = code {
-                return Some(*c)
+        if let Self::KeyEvent(key_event) = self {
+            if let crossterm::event::KeyCode::Char(char) = key_event.code {
+                if key_event.modifiers.contains(crossterm::event::KeyModifiers::SHIFT) {
+                    return Some(char.to_ascii_uppercase());
+                }
+                return Some(char);
             }
         }
-        None
+        return None;
     }
     pub fn is_enter(&self) -> bool {
-        if let UserInput::KeyPress(code) = self {
-            if let crossterm::event::KeyCode::Enter = code {
-                return true
+        if let UserInput::KeyEvent(code) = self {
+            if let crossterm::event::KeyCode::Enter = code.code {
+                return true;
             }
         }
-        return false
+        return false;
     }
 }
 
