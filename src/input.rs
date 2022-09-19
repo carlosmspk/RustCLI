ec40a4b79340fef2f017ed28de4cef7efc3b0a63
carlosmspk <carlos_cp_santos@hotmail.com>
2022-09-19 20:30:04 +0100

    Add UserInput struct to wrap around crossterm events, numbers, and strings

---
+++ src/input.rs
@@ -0,0 +1,26 @@
+pub enum UserInput {
+    Number(i64),
+    Text(String),
+    KeyPress(crossterm::event::KeyCode),
+    Mouse(crossterm::event::MouseEventKind),
+    Other(crossterm::event::Event),
+}
+
+impl UserInput {
+    pub fn as_char(&self) -> Option<char> {
+        if let Self::KeyPress(code) = self {
+            if let crossterm::event::KeyCode::Char(c) = code {
+                return Some(*c)
+            }
+        }
+        None
+    }
+    pub fn is_enter(&self) -> bool {
+        if let UserInput::KeyPress(code) = self {
+            if let crossterm::event::KeyCode::Enter = code {
+                return true
+            }
+        }
+        return false
+    }
+}
