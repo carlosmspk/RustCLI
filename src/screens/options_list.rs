8ce62754e6614f2d21db016d49982d63334e9095
carlosmspk <carlos_cp_santos@hotmail.com>
2022-10-21 18:34:49 +0100

    Incomplete: implement OptionList

--- src/screens/options_list.rs
+++ src/screens/options_list.rs
@@ -1,17 +1,51 @@
+use crossterm::event::{KeyCode, KeyEventKind};
+
 use crate::{error::Error, text::AnyString};
 
 use super::{Screen, ScreenCommand};
 
 pub struct OptionsList {
     options: Vec<AnyString>,
+    enumerate: bool,
+    currently_selected: usize,
 }
 
 impl OptionsList {
-    pub fn new(options: Vec<AnyString>) -> Result<Self, Error> {
+    pub fn from_any_string(options: Vec<AnyString>) -> Result<Self, Error> {
         if options.len() == 0 {
             return Err(Error::ItemlessOptionListError);
         }
-        Ok(Self { options })
+        Ok(Self {
+            options,
+            enumerate: false,
+            currently_selected: 0,
+        })
+    }
+    pub fn from_string(options: Vec<String>) -> Result<Self, Error> {
+        if options.len() == 0 {
+            return Err(Error::ItemlessOptionListError);
+        }
+        let casted_options = options.iter().map(|e| e.into()).collect();
+        Ok(Self {
+            options: casted_options,
+            enumerate: false,
+            currently_selected: 0,
+        })
+    }
+    pub fn from<T>(options: Vec<T>) -> Result<Self, Error> where T:ToString {
+        if options.len() == 0 {
+            return Err(Error::ItemlessOptionListError);
+        }
+        let casted_options = options.iter().map(|e| e.to_string().into()).collect();
+        Ok(Self {
+            options: casted_options,
+            enumerate: false,
+            currently_selected: 0,
+        })
+    }
+    pub fn enumerate_items(mut self) -> Self {
+        self.enumerate=true;
+        self
     }
 }
 
@@ -21,7 +55,11 @@ impl Screen for OptionsList {
     }
 
     fn on_event(&mut self, input: crate::input::UserInput) -> Result<ScreenCommand, Error> {
-        todo!()
+        enum MoveCommand{
+            UP,
+            DOWN
+        };
+        Ok(ScreenCommand::Idle)
     }
 
     fn on_screen_exit(&self) -> Option<crate::input::UserInput> {
